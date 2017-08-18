8debe027cc54d374a4cc8191b3df0ac6ac2d6075
Ingo Albers <ingo.albers@live.de>
2017-08-18 12:41:51 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'celluloid-io'
+gem 'giphy'
+gem 'slack-ruby-bot'
---
+++ funnybot.rb
@@ -0,0 +1,17 @@
+require 'open-uri'
+require 'slack-ruby-bot'
+
+class Funnybot < SlackRubyBot::Bot
+  scan('joke') do |client, data, _match|
+    response = open('https://icanhazdadjoke.com/', 'Accept' => 'application/json').read
+    joke = JSON.parse(response)['joke']
+
+    client.say(text: joke, channel: data.channel)
+  end
+
+  match(/^gif (?<context>\w*)$/) do |client, data, match|
+    client.say(channel: data.channel, gif: match[:context])
+  end
+end
+
+Funnybot.run
